704113e7bfb0a7f68f4c759b6dac31f380c5205c
Sam Zimmerman <samzimmerman95@gmail.com>
2025-11-25 18:02:32 -0800

    Use firebase admin to adjust some faulty temp entries and update .gitignore

--- FirebaseAdmin/admin.py
+++ FirebaseAdmin/admin.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('temperat-fea82-firebase-adminsdk-gk4pn-97d4eda3b1.json')
+cred = credentials.Certificate('temperat-fea82-firebase-adminsdk-gk4pn-3feb811dc0.secret.json')
 
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
@@ -11,14 +11,14 @@ firebase_admin.initialize_app(cred, {
 })
 
 # As an admin, the app has access to read and write all data, regardless of Security Rules
-ref = db.reference('lake23')
-lake23 = ref.get()
-print(len(lake23))
+ref = db.reference('test2')
+all_data = ref.get()
+print(len(all_data))
 res = 0
-for key, value in lake23.items():
-    if int(key) < 1684788739000:
+for key, value in all_data.items():
+    if int(value[1]) > 170:
         res += 1
-        # ref.child(key).set({})
+        ref.child(key).set([value[0], value[0]])
 print(res)
 # print(ref.child('1684726299000').set({}))
 
