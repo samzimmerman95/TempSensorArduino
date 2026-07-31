fa7c8f91dd9e79449ade1db56b74547307db12eb
samzimmerman95 <samzimmerman95@gmail.com>
2026-07-30 20:01:21 -0700

    Fix secret key reference

--- FirebaseAdmin/admin.py
+++ FirebaseAdmin/admin.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('temperat-fea82-firebase-adminsdk-gk4pn-3feb811dc0.secret.json')
+cred = credentials.Certificate('temperat-fea82-firebase-adminsdk-gk4pn-97d4eda3b1.secret.json')
 
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
